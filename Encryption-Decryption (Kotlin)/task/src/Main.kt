fd6e256fd9622b5b3491333688ccff1cb9f8e227
Sanders <sndrs90@gmail.com>
2025-01-09 09:40:07 +0300

    Stage 3 completed

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,20 +1,27 @@
 package encryptdecrypt
 
-const val ABC = "abcdefghijklmnopqrstuvwxyz"
-
 fun main() {
+    val operation = readln()
     val message = readln()
     val key = readln().toInt()
+    when (operation) {
+        "enc" -> encryptMessage(message, key)
+        "dec" -> decryptMessage(message, key)
+    }
+}
 
+private fun encryptMessage(message: String, key: Int) {
     val ciphertext = message.toCharArray()
     message.forEachIndexed { index, ch ->
-        if (ch.isLetter()) {
-            val ind = ABC.indexOf(ch)
-            ciphertext[index] = when {
-                ind + key < ABC.length -> ABC[ind + key]
-                else -> ABC[ind + key - 26]
-            }
-        }
+        ciphertext[index] = (ch.code + key).toChar()
     }
     println(ciphertext)
+}
+
+private fun decryptMessage(ciphertext: String, key: Int) {
+    val message = ciphertext.toCharArray()
+    ciphertext.forEachIndexed { index, ch ->
+        message[index] = (ch.code - key).toChar()
+    }
+    println(message)
 }
